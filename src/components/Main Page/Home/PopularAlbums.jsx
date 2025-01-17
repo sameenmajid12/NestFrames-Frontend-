bbf34bfd857b82d530b3c1a0392f5b12487b4d62
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-01-17 02:37:44 -0500

    Created popular albums component for the home page

--- src/components/Main Page/Home/PopularAlbums.jsx
+++ src/components/Main Page/Home/PopularAlbums.jsx
@@ -1,7 +1,23 @@
-function PopularAlbums() {
+function PopularAlbums({albums}) {
   return (
-    <div className="home-side-container">
-      <h1 className="home-side-header">Popular Albums</h1>
+    <div className="popular-albums-container">
+      <h1 className="popular-albums-header">Popular Albums</h1>
+      <div className="popular-albums-body">
+        {albums.map((album)=>{
+          return(<div key={album._id} className="popular-album">
+            <div className="popular-album-details">
+              <h2 className="popular-album-name">{album.name}</h2>
+              <div className="popular-album-stats-container">
+                <p className="popular-album-stat">{album.users.length} collaborators</p>
+                <p className="popular-album-stat">{album.likes} likes</p>
+                <p className="popular-album-stat">{album.posts.length} posts</p>
+              </div>
+            </div>
+              <img className="popular-album-cover" src={album.coverPhoto.fileUrl}></img>
+          </div>)
+          
+        })}
+      </div>
     </div>
   );
 }
