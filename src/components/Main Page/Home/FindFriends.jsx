7694cf48c3f1dbbea69d5d000404de7449517fdf
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-01-17 02:37:19 -0500

    Created find friends component for the home page

--- src/components/Main Page/Home/FindFriends.jsx
+++ src/components/Main Page/Home/FindFriends.jsx
@@ -1,8 +1,25 @@
-function FindFriends() {
+function FindFriends({ friends }) {
+  console.log(friends);
   return (
-    <div className="home-side-container">
-      <div className="find-friends">
-        <h1 className="find-friends-header">Find Friends</h1>
+    <div className="find-friends-container">
+      <h1 className="find-friends-header">Find Friends</h1>
+      <div className="find-friends-body">
+        {friends.map((friend) => {
+          return(
+          <div key={friend._id} className="find-friend">
+            <div className="find-friend-details">
+              <img
+                className="find-friend-image"
+                src={friend.profilePic.fileUrl}
+              ></img>
+              <div className="find-friend-name">
+                <h3>{friend.fullname}</h3>
+                <p>{friend.username}</p>
+              </div>
+            </div>
+            <button className="find-friend-add">Add</button>
+          </div>);
+        })}
       </div>
     </div>
   );
