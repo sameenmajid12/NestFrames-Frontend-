44e5009080b208079f92346bcf00c35fc871e0e1
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-03-19 17:55:47 -0400

    Notification components cleanup

--- src/components/Main Page/Contexts/NotificationContext.jsx
+++ src/components/Main Page/Contexts/NotificationContext.jsx
@@ -6,33 +6,41 @@ import SentNotification from "../Notifications/SentNotification";
 export const NotificationContext = createContext();
 
 export const NotificationProvider = ({ children }) => {
-  const [sentNotifications, setSentNotifications] = useState([]); 
-  const [receivedNotifications, setReceivedNotifications] = useState([]); 
+  const [sentNotifications, setSentNotifications] = useState([]);
+  const [receivedNotifications, setReceivedNotifications] = useState([]);
   const { user } = useContext(UserContext);
   const { socket } = useContext(SocketContext);
-  const fetchNotifications = async()=>{
-    const response = await fetch(`http://localhost:3002/notifications/${user._id}/getNotifications`);
+  const fetchNotifications = async () => {
+    const response = await fetch(
+      `http://localhost:3002/notifications/${user._id}/getNotifications`
+    );
     console.log(response);
-    if(response.ok){
-      const {notifications} = await response.json();
+    if (response.ok) {
+      const { notifications } = await response.json();
       setReceivedNotifications(notifications);
     }
   };
-  useEffect(()=>{
-    if(user){
+  useEffect(() => {
+    if (user) {
       fetchNotifications();
     }
-  },[user])
+  }, [user]);
 
-  let timeoutId;
+  let timeoutId = null;
   const addSentNotification = (success, message) => {
-    clearTimeout(timeoutId);
-    setSentNotifications((prev) => {console.log([...prev,{success,message}]); return[...prev, {success, message}]});
+    if(timeoutId)clearTimeout(timeoutId);
+    setSentNotifications((prev) => {
+      return [...prev, { success, message }];
+    });
     timeoutId = setTimeout(() => {
       setSentNotifications((prev) => prev.slice(1));
     }, 5000);
   };
-
+  const removeSentNotification = () => {
+    clearTimeout(timeoutId);
+    setSentNotifications((prev) => prev.slice(1));
+    timeoutId=null;
+  };
   const addReceivedNotification = (notification) => {
     setReceivedNotifications((prev) => {
       return Array.isArray(prev) ? [notification, ...prev] : [notification];
@@ -57,13 +65,18 @@ export const NotificationProvider = ({ children }) => {
     addReceivedNotification,
     receivedNotifications,
     setReceivedNotifications,
+    removeSentNotification,
   };
 
   return (
     <NotificationContext.Provider value={value}>
       {children}
       {sentNotifications.length > 0 && (
-        <SentNotification success={sentNotifications[0].success} message={sentNotifications[0].message} />
+        <SentNotification
+          success={sentNotifications[0].success}
+          message={sentNotifications[0].message}
+          removeNotification={removeSentNotification}
+        />
       )}
     </NotificationContext.Provider>
   );
--- src/components/Main Page/Notifications/SentNotification.jsx
+++ src/components/Main Page/Notifications/SentNotification.jsx
@@ -1,19 +1,45 @@
-
-function SentNotification({success,message, setNotifications}) {
-  return(
-    <div className={`notification-container notification-${success?"success":"error"}`}>
+function SentNotification({ success, message, removeNotification }) {
+  return (
+    <div
+      className={`notification-container notification-${
+        success ? "success" : "error"
+      }`}
+    >
       <div className="notification-left">
-        <i className={`fa-solid fa-${success?"circle-check success-icon":"circle-xmark error-icon"}`}></i>
+        <i
+          className={`fa-solid fa-${
+            success ? "circle-check success-icon" : "circle-xmark error-icon"
+          }`}
+        ></i>
         <div className="notification-message">
-          <p className={`${success?"success-":"error-"}notification-message-head`}>{success?"Success":"Error"}</p>
-          <p className={`${success?"success-":"error-"}notification-message-message`}>{message}</p>
+          <p
+            className={`${
+              success ? "success-" : "error-"
+            }notification-message-head`}
+          >
+            {success ? "Success" : "Error"}
+          </p>
+          <p
+            className={`${
+              success ? "success-" : "error-"
+            }notification-message-message`}
+          >
+            {message}
+          </p>
         </div>
       </div>
-      <div className={`notification-right ${success?"success-":"error-"}right`}>
-      <i onClick={()=>setNotifications(prev=>prev.slice(1))} className={`fa-solid fa-xmark ${success?"success-":"error-"}xmark`}></i>
+      <div
+        className={`notification-right ${success ? "success-" : "error-"}right`}
+        onClick={() => removeNotification()}
+      >
+        <i
+          className={`fa-solid fa-xmark ${
+            success ? "success-" : "error-"
+          }xmark`}
+        ></i>
       </div>
     </div>
-  )
+  );
 }
 
 export default SentNotification;
