4079c16b5e16785065d4d5ded3f0ad0c63aad25c
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-03-23 04:09:09 -0400

    Implemented useMessageActions hook for friend message button

--- src/components/Main Page/Friends/FriendsContent.jsx
+++ src/components/Main Page/Friends/FriendsContent.jsx
@@ -1,44 +1,16 @@
-import { useContext } from "react";
 import FriendsCSS from "../../../styles/friends.module.css";
 import Loading from "../Utils/Loading";
-import { useNavigate, useOutletContext } from "react-router-dom";
-import { UserContext } from "../Contexts/UserContext";
-import { AuthContext } from "../Contexts/AuthContext";
+import useFriendsActions from "../../../hooks/useFriendsActions";
+import { useOutletContext } from "react-router-dom";
 function FriendsContent({ friendProp }) {
-  const { user, setUser } = useContext(UserContext);
-  const { token } = useContext(AuthContext);
+  const {messageFriend} = useFriendsActions();
   const {friends: contextFriends } = useOutletContext() || {};
   const {searchRef} = useOutletContext();
   let friends = contextFriends || friendProp;
-  const navigate = useNavigate();
   if (!friends) {
     return <Loading />;
   }
-  const message = async (friend) => {
-    const response = await fetch(
-      `http://localhost:3002/messages/${user._id}/message/${friend._id}`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-    if (!response.ok) {
-      throw new Error("Failed to retrieve or create conversation");
-    }
-    const { newConversation, conversation, message } = await response.json();
-    if (newConversation) {
-      const updatedConversations = [...user.conversations, newConversation];
-      setUser((prev) => {
-        console.log(prev);
-        return { ...prev, conversations: updatedConversations };
-      });
-      navigate(`/messages/${newConversation._id}`);
-      return;
-    }
-    navigate(`/Messages/${conversation._id}`);
-  };
+ 
   const focusSearch=()=>{
     if(searchRef.current){
       searchRef.current.focus();
@@ -66,7 +38,7 @@ function FriendsContent({ friendProp }) {
               </div>
               <div className={FriendsCSS.friendButtons}>
                 <button
-                  onClick={() => message(friend)}
+                  onClick={() => messageFriend(friend._id)}
                   className={FriendsCSS.messageButton}
                 >
                   {friendProp ? "Add" : "Message"}
